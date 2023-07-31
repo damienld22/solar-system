c98e157c7c59265f5195972cec87c82709d3d801
Damien Le Dantec <damien-le-dantec@hotmail.fr>
2023-07-31 21:50:12 +0200

    chore: Built version

--- scripts/textures/background.js
+++ scripts/textures/background.js
@@ -1,6 +1,6 @@
 import * as THREE from "three";
 
-const texture = new THREE.TextureLoader().load("../../assets/night-sky.jpg");
+const texture = new THREE.TextureLoader().load("/assets/night-sky.jpg");
 texture.wrapS = THREE.RepeatWrapping;
 texture.wrapT = THREE.RepeatWrapping;
 
